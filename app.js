70dfd6a4c40db8e454b12688de0ed374a96f509c
Deni Maulana <denimlnm@gmail.com>
2023-02-04 22:26:58 +0700

    modifikasi game

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
         
                 document.getElementById('score').innerHTML = parseInt(document.getElementById('score').innerHTML) + 1;
                 setBackgroundMoving();
-            }, 3.5);
+            }, 3);
         }
         
     }
@@ -23,22 +23,22 @@
 
         setTimeout(function() {
             box.style.marginLeft = (parseInt(box.style.marginLeft.replace('px', '')) -1) +'px';
-            if(parseInt(box.style.marginLeft.replace('px', '')) < -70){
-                box.style.marginLeft = '700px';
+            if(parseInt(box.style.marginLeft.replace('px', '')) < -60){
+                box.style.marginLeft = '1500px';
             }
 
-            if(dino.offsetTop + 50 >= box.offsetTop &&
-                dino.offsetLeft + 50 >= box.offsetLeft &&
-                dino.offsetTop + 50 <= box.offsetTop + 50 &&
-                dino.offsetLeft <= box.offsetLeft + 50) {
+            if(dino.offsetTop + 70 >= box.offsetTop &&
+                dino.offsetLeft + 70 >= box.offsetLeft &&
+                dino.offsetTop + 70 <= box.offsetTop + 70 &&
+                dino.offsetLeft <= box.offsetLeft + 70) {
                     dino.setAttribute('class', 'freeze');
                     isMoving = false;
-                    alert('game over')
+                    alert('game over score kamu : '+ document.getElementById('score').innerHTML)
             } else {
                 setBoxMoving();
             }
             
-        }, 3.5);
+        }, 3);
     }
     setBoxMoving();
 
@@ -46,10 +46,10 @@
 
     window.addEventListener('keyup', function(e) {
         if (e.keyCode == 32){
-            this.document.getElementById('dino').style.marginTop = '100px';
+            this.document.getElementById('dino').style.marginTop = '70px';
             this.document.getElementById('dino').setAttribute('class' , 'freeze');
             this.setTimeout(function(){
-                this.document.getElementById('dino').style.marginTop = '250px';
+                this.document.getElementById('dino').style.marginTop = '350px';
                 this.document.getElementById('dino').setAttribute('class' , '');
             }, 1000)
         }
